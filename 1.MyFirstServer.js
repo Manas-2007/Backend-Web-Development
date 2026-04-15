0966b0bcd792d0b01dbf4fef6d9c9043b0da90b6
Manas-2007 <patidarmanas01@gmail.com>
2026-04-15 07:42:04 +0530

    Server interaction Revision

--- 1.MyFirstServer.js
+++ 1.MyFirstServer.js
@@ -1,39 +1,20 @@
-//Using router
-const http = require('http');
+const http=require('http');
 const server=http.createServer((req,res)=>{
-    console.log(req.headers,req.url);
+    console.log(req.url,req.method);
 
-    //Routing Response
-    if(req.url==='/')
-    {
-        res.setHeader("Content-Type","text/html");
-        res.write('<html>');
-        res.write('<head><title>Server Designing</title></head>')
-        res.write('<body><h1>Welcome to Home Page....</h1></body>')
-        res.write('</html>');
+    //Webpage interaction
+    res.setHeader('Content-Type','text/html');
+    res.write(`
+        <html>
+        <head><title>My First Server</title></head>
+        <body><h2>Welcome to My First Node Server.....!</h2></body>
+        </html>     
+        `)
         return res.end();
-    }
-    else if(req.url==='/products')
-    {
-        res.setHeader("Content-Type","text/html");
-        res.write('<html>');
-        res.write('<head><title>Server Designing</title></head>')
-        res.write('<body><h1>Product Cart is Empty! Try again later..........</h1></body>')
-        res.write('</html>');
-        return res.end();   
-    }
-    else
-    {
-        res.setHeader("Content-Type","text/html");
-        res.write('<html>');
-        res.write('<head><title>Server Designing</title></head>')
-        res.write('<body><h1>This is the End of the Webpage...</h1></body>')
-        res.write('</html>');
-        return res.end();
-    }
-});
+})
 
-const PORT=1000;
-server.listen(PORT,()=>{
-    console.log(`http://localhost:${PORT}`);
-});
+//Request from the server
+const port=2000;
+server.listen(port,()=>{
+    console.log(`http://localhost:${port}`);
+})
