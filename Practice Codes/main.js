a886f35c8981a617ce3e9f5758699af0d178db41
Manas-2007 <patidarmanas01@gmail.com>
2026-04-21 17:27:25 +0530

    Created first practice form

--- Practice Codes/main.js
+++ Practice Codes/main.js
@@ -1,10 +1,28 @@
 const express=require('express');
+const path=require('path');
 const app=express();
 const PORT=3000;
-app.use((req,res)=>{
-    res.send("Hello world");
-})
-
 app.listen(PORT,()=>{
-    console.log(`Server is Live at http://localhost:${PORT}`);
+    console.log(`Server Live at http://localhost:${PORT}`);
 });
+
+//Displaying form
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'));
+});
+
+//Reading the data from the form
+app.use(express.urlencoded({extended:true}));
+
+//Displaying information 
+let users=[];
+app.post('/save',(req,res)=>{
+    users.push({
+        Name : req.body.name,
+        Password:req.body.password,
+        E_Mail:req.body.email,
+        Contact:req.body_contact,
+    })
+    console.log(users);
+    res.send(users);
+})
